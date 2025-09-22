845c6ff89a6a648686399691ae7457fd5a734504
deomiarn <deomicarnel@hotmail.com>
2025-09-22 11:14:18 +0200

    PS2.2

--- 2 Compiler 1/src/Calculator.java
+++ 2 Compiler 1/src/Calculator.java
@@ -1,26 +1,41 @@
+import java.util.Stack;
+
 /**
  * User: Karl Rege
  */
 
 
 public class Calculator {
+    private static Stack<Double> stack = new Stack<>();
 
-    public static void expr()	throws Exception {
+    public static void expr() throws Exception {
         term();
         while (Scanner.la == Token.PLUS
                 || Scanner.la == Token.MINUS) {
             Scanner.scan();
             int op = Scanner.token.kind;
             term();
+            if (op == Token.PLUS) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (op == Token.MINUS) {
+                stack.push(-stack.pop() + stack.pop());
+            }
         }
     }
 
-    public static void term()	throws Exception {
+    public static void term() throws Exception {
         factor();
         while (Scanner.la == Token.TIMES || Scanner.la == Token.SLASH) {
             Scanner.scan();
             int op = Scanner.token.kind;
             factor();
+            if (op == Token.SLASH) {
+                Double divisor = stack.pop();
+                Double dividend = stack.pop();
+                stack.push(dividend / divisor);
+            } else if (op == Token.TIMES) {
+                stack.push(stack.pop() * stack.pop());
+            }
         }
     }
 
@@ -31,6 +46,7 @@ public class Calculator {
             Scanner.check(Token.RBRACK);
         } else if (Scanner.la == Token.NUMBER) {
             Scanner.scan();
+            stack.push(Scanner.token.val);
         }
     }
 
@@ -38,8 +54,9 @@ public class Calculator {
         Scanner.init(expr);
         Scanner.scan();
         expr();
+
         // return result
-        return 0;
+        return stack.pop();
     }
 
 
